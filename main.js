915861098059345ad20e79ead26c87640d089a13
Jia Qian <isjia@163.com>
2016-05-20 18:31:15 +0800

    add pipes and make them move left

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ var mainState = {
 
     // Load the bird sprite
     game.load.image('bird', 'assets/bird.png');
+
+    // Load the pipe sprite
+    game.load.image('pipe', 'assets/pipe.png');
   },
 
   create: function(){
@@ -31,6 +34,11 @@ var mainState = {
     // Call the 'jump' function when the spaceKey is hit
     var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     spaceKey.onDown.add(this.jump, this);
+
+    // Create an empty group
+    this.pipes = game.add.group();
+
+    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
   },
 
   update: function(){
@@ -54,6 +62,38 @@ var mainState = {
   restartGame: function(){
     // Start the 'main' state, which restarts the game.
     game.state.start('main');
+  },
+
+  addOnePipe: function(x, y){
+    // Create a pipe at the position x and y
+    var pipe = game.add.sprite(x, y, 'pipe');
+
+    // Add the pipe to our previously created group
+    this.pipes.add(pipe);
+
+    // Enable physics on the pipe
+    game.physics.arcade.enable(pipe);
+
+    // Add velocity to the pipe to make it move left
+    pipe.body.velocity.x = -200;
+
+    // Automatically kill the pipe when it's no longer visible
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+  },
+
+  addRowOfPipes: function(){
+    // Randomly pick a number between 1 and 5
+    // This will be the hole position
+    var hole = Math.floor(Math.random()*5)+1;
+
+    // Add the 6 pipes
+    // With one big hole at position 'hole' and 'hole+1'
+    for (var i=0; i < 8; i++) {
+      if (i!= hole && i!=hole+1) {
+        this.addOnePipe(400, i*60+10);
+      }
+    }
   },
 };
 
